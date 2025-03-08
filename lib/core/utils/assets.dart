c3b1b16ab59a84f04c2b2bd36b8ecc72a77695f6
Mohammed Al-mahmoud <Mohammed Al-mahmoud>
2025-03-08 14:55:02 +0300

    add styles file

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,3 +1,5 @@
 class AssetsData {
   static const almarai = 'assets/fonts/Almarai-Regular.ttf';
+   static const everett = 'assets/fonts/Everett Regular.ttf';
+   static const poppins = 'assets/fonts/Poppins-Regular.ttf';
 }
