6f8ac3b4ccb3a2998de8396f482aebd483356a45
Mohammed Al-mahmoud <Mohammed Al-mahmoud>
2025-03-09 14:51:44 +0300

    Add border to UI elements

--- lib/Features/home_view/presentation/home_view.dart
+++ lib/Features/home_view/presentation/home_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:subul_dashboard/core/utils/app_colors.dart';
+import 'package:subul_dashboard/core/utils/assets.dart';
 import 'package:subul_dashboard/core/widgets/text_logo.dart';
 
 class HomeView extends StatelessWidget {
@@ -8,21 +10,29 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(image: DecorationImage(image: SvgPicture.asset(assetName))),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 91, top: 51),
-                  child: TextLogo(),
+      body: ListView(
+        children: [
+          Stack(
+            children: [
+              Center(child: SvgPicture.asset(AssetsData.background1)),
+              Padding(
+                padding: const EdgeInsets.only(left: 91, top: 51),
+                child: TextLogo(),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 39, top: 148, bottom: 32),
+                child: Container(
+                  width: 365,
+                  height: 844,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: AppColors.deepPurple, width: 2),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
                 ),
-              ],
-            ),
-          ],
-        ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
