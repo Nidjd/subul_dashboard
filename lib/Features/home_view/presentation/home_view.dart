315421113afd9828da6c624aa6b47e4030c2db20
Mohammed Al-mahmoud <Mohammed Al-mahmoud>
2025-03-09 16:05:54 +0300

    Add toggle button for switching between company and client

--- lib/Features/home_view/presentation/home_view.dart
+++ lib/Features/home_view/presentation/home_view.dart
@@ -2,11 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:subul_dashboard/core/utils/app_colors.dart';
 import 'package:subul_dashboard/core/utils/assets.dart';
+import 'package:subul_dashboard/core/utils/styles.dart';
 import 'package:subul_dashboard/core/widgets/text_logo.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  bool isSelected = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,6 +34,98 @@ class HomeView extends StatelessWidget {
                   decoration: BoxDecoration(
                     border: Border.all(color: AppColors.deepPurple, width: 2),
                     borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 75,
+                          left: 38,
+                          right: 48,
+                        ),
+                        child: Container(
+                          width: 279,
+                          height: 71,
+                          decoration: BoxDecoration(
+                            color: AppColors.goldenYellow,
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    isSelected = false;
+                                  });
+                                },
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                    left: 8,
+                                    top: 5,
+                                    bottom: 5,
+                                  ),
+                                  child: Container(
+                                    width: 125,
+                                    height: 61,
+                                    decoration: BoxDecoration(
+                                      color:
+                                          isSelected
+                                              ? AppColors.goldenYellow
+                                              : AppColors.deepPurple,
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        'شركة',
+
+                                        style: Styles.textStyle20.copyWith(
+                                          color: AppColors.white,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    isSelected = true;
+                                  });
+                                },
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                    top: 5,
+                                    bottom: 5,
+                                    right: 8,
+                                  ),
+                                  child: Container(
+                                    width: 125,
+                                    height: 61,
+                                    decoration: BoxDecoration(
+                                      color:
+                                          isSelected
+                                              ? AppColors.deepPurple
+                                              : AppColors.goldenYellow,
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        'عميل',
+
+                                        style: Styles.textStyle20.copyWith(
+                                          color: AppColors.white,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -3,4 +3,5 @@ class AssetsData {
    static const everett = 'assets/fonts/Everett Regular.ttf';
    static const poppins = 'assets/fonts/Poppins-Regular.ttf';
    static const background1 = 'assets/images/background1.svg';
+   static const smallCar ='assets/images/small car.svg';
 }
